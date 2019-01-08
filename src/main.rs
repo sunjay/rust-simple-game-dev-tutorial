fc654bfc69c49d38b152e061a2794a4039dab470
Sunjay Varma <varma.sunjay@gmail.com>
2019-01-08 12:17:29 -0500

    Getting just a single sprite to render

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Point, Rect};
 // "self" imports the "image" module itself as well as everything else we listed
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
 
     canvas.present();
 
@@ -36,6 +48,10 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/raptor.png")?;
 
+    let position = Point::new(0, 0);
+    // src position in the spritesheet
+    let sprite = Rect::new(0, 0, 94, 100);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
@@ -54,7 +70,7 @@ fn main() -> Result<(), String> {
 
         // Render
         i = (i + 1) % 255;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
 
         // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
