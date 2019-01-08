e5858df5205598655b5cdbd4aec04644a74ee0dd
Sunjay Varma <varma.sunjay@gmail.com>
2019-01-08 11:02:59 -0500

    Restructured into traditional game loop

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::WindowCanvas;
 use std::time::Duration;
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem.window("game tutorial", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make a canvas");
+
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -32,9 +35,16 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        //TODO: Update
+
+        // Render
+        i = (i + 1) % 255;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+
+        // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+
+    Ok(())
 }
