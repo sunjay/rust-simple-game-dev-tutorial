f79f8591affc8b2043c04cfad3a1b9dbda437b32
Sunjay Varma <varma.sunjay@gmail.com>
2019-01-08 11:39:22 -0500

    Getting a basic texture rendering

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
+use sdl2::render::{WindowCanvas, Texture};
+// "self" imports the "image" module itself as well as everything else we listed
+use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    canvas.copy(texture, None, None)?;
+
     canvas.present();
+
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
+    // stay unused because if we don't have any variable at all then Rust will treat it as a
+    // temporary value and drop it right away!
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window = video_subsystem.window("game tutorial", 800, 600)
         .position_centered()
@@ -21,6 +32,9 @@ fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/raptor.png")?;
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -40,7 +54,7 @@ fn main() -> Result<(), String> {
 
         // Render
         i = (i + 1) % 255;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
 
         // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
