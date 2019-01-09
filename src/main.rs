59bf2dd01fc5cf6aadf021689eb33e10520f5d22
Sunjay Varma <varma.sunjay@gmail.com>
2019-01-08 21:59:58 -0500

    Animating the player in the direction they are walking

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,18 @@ struct Player {
     sprite: Rect,
     speed: i32,
     direction: Direction,
+    current_frame: i32,
+}
+
+/// Returns the row of the spritesheet corresponding to the given direction
+fn direction_spritesheet_row(direction: Direction) -> i32 {
+    use self::Direction::*;
+    match direction {
+        Up => 3,
+        Down => 0,
+        Left => 1,
+        Right => 2,
+    }
 }
 
 fn render(
@@ -36,10 +48,18 @@ fn render(
 
     let (width, height) = canvas.output_size()?;
 
+    let (frame_width, frame_height) = player.sprite.size();
+    let current_frame = Rect::new(
+        player.sprite.x() + frame_width as i32 * player.current_frame,
+        player.sprite.y() + frame_height as  i32 * direction_spritesheet_row(player.direction),
+        frame_width,
+        frame_height,
+    );
+
     // Treat the center of the screen as the (0, 0) coordinate
     let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
-    canvas.copy(texture, player.sprite, screen_rect)?;
+    let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
+    canvas.copy(texture, current_frame, screen_rect)?;
 
     canvas.present();
 
@@ -64,6 +84,12 @@ fn update_player(player: &mut Player) {
         Down => {
             player.position = player.position.offset(0, player.speed);
         },
+    }
+
+    // Only continue to animate if the player is moving
+    if player.speed != 0 {
+        // Cheat: using the fact that all animations are 3 frames (NOT extensible)
+        player.current_frame = (player.current_frame + 1) % 3;
     }
 }
 
@@ -91,6 +117,7 @@ fn main() -> Result<(), String> {
         sprite: Rect::new(0, 0, 94, 100),
         speed: 0,
         direction: Direction::Right,
+        current_frame: 0,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
